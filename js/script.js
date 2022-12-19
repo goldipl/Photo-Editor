82ea12a9b8f58a46b70386719cf7f005f6809f54
goldipl <mgodlewski92@gmail.com>
2022-12-19 19:40:56 +0100

    handle adding images

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const fileUpload = document.getElementById("file-upload");
+const addImgBtn = document.querySelector(".add-image-btn");
+const showImg = document.querySelector(".right_container .image_container img");
+
+addImgBtn.addEventListener("click", () => fileUpload.click());
+
+const loadImg = () => {
+    // get selected image
+    let img = fileUpload.files[0];
+    // return if image hasn't selected
+    if(!img) return;
+    showImg.src = URL.createObjectURL(img);
+}
+
+fileUpload.addEventListener("change", loadImg);
